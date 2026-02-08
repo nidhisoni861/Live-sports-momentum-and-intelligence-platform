ecd8d972ca264b1dade9cee7c563fd15a691b6d9
Naman Patel <atharvkunchage2910@gmail.com>
2026-02-08 22:50:30 +0100

    Atharv_Mongo_unique_fix

--- app/db/mongo.ts
+++ app/db/mongo.ts
@@ -74,7 +74,7 @@ async function ensureIndexes() {
 }
 
 /* ======================================================
-   UPSERT LOGICCC – CREATED AT ONLY ON INSERT
+   UPSERT LOGICCCC – CREATED AT ONLY ON INSERT
    ====================================================== */
 export async function saveVideoAnalysis(data: VideoAnalysisDoc) {
   const db = await getDb();
