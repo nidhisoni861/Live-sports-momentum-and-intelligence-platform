1ae63f05201b9f7ed2e88cb36f7d934dabfdaaa5
nidhisoni861 <nidhisoni861@gmail.com>
2026-02-08 22:10:20 +0530

    add the final design to backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-
-import VideoTest from  '@/app/components/VideoTest';
+import VideoTest from "@/app/components/VideoTest";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-zinc-50 dark:bg-black py-12 px-4 sm:px-6 lg:px-8">
-      <main className="max-w-7xl mx-auto space-y-8">
-     
+    <div className="min-h-screen bg-[var(--background)] py-6 px-4 sm:px-6 lg:px-8">
+      <main className="max-w-[1400px] mx-auto">
         <VideoTest />
       </main>
     </div>
